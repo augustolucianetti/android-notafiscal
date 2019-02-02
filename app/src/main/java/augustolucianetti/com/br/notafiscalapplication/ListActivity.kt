2a4a4ab417f888a39c18c9fb2f6a76b7750fcb35
Augusto César Favaretto Lucianetti <augusto_lucianetti@hotmail.de>
2019-02-02 20:56:03 -0200

    tratamento quando não existem nostas fiscais cadastradas e criação de mensagem de confirmação no momento da exclusao

--- app/src/main/java/augustolucianetti/com/br/notafiscalapplication/ListActivity.kt
+++ app/src/main/java/augustolucianetti/com/br/notafiscalapplication/ListActivity.kt
@@ -4,10 +4,12 @@ import android.app.Activity
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AlertDialog
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.OrientationHelper
 import android.support.v7.widget.RecyclerView
 import android.view.View
+import android.view.ViewGroup
 import android.widget.Toast
 import augustolucianetti.com.br.notafiscalapplication.model.NotaFiscal
 import com.firebase.ui.database.FirebaseRecyclerAdapter
@@ -15,9 +17,15 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.Query
+import com.google.firebase.database.ValueEventListener
 import kotlinx.android.synthetic.main.activity_list.*
 import kotlinx.android.synthetic.main.reciclerview_item_row.*
 import kotlinx.android.synthetic.main.reciclerview_item_row.view.*
+import com.google.firebase.database.DatabaseError
+import java.nio.file.Files.exists
+import com.google.firebase.database.DataSnapshot
+
+
 
 
 class ListActivity : AppCompatActivity() {
@@ -69,16 +77,45 @@ class ListActivity : AppCompatActivity() {
                 viewHolder?.itemView?.ibtnDelete?.setOnClickListener {
                     System.out.println("Nota fiscal:" + position)
                     System.out.println("tabela" + tabela)
-                    FirebaseDatabase.getInstance().getReference(getString(R.string.nomeTabelaNotaFiscal)).child(notaFiscal.id).removeValue().addOnCompleteListener{
-                        if (it.isSuccessful) {
-                            Toast.makeText(this@ListActivity,
-                                getString(R.string.mensagem_nota_fiscal_excluida_sucesso), Toast.LENGTH_SHORT).show()
-                        } else {
-                            System.out.println(getString(R.string.erro_firebase) + it.exception)
-                            Toast.makeText(this@ListActivity,
-                                it.exception?.message, Toast.LENGTH_SHORT).show()
+
+                    val builder = AlertDialog.Builder(this@ListActivity)
+
+                    builder.setTitle(getString(R.string.atencao))
+                    builder.setMessage(getString(R.string.mensagem_confirmaar_exclusao_nota_fiscal))
+                    builder.setPositiveButton(getString(R.string.ok)) { dialog, which ->
+
+                        FirebaseDatabase.getInstance().getReference(getString(R.string.nomeTabelaNotaFiscal)).child(notaFiscal.id).removeValue().addOnCompleteListener{
+                            if (it.isSuccessful) {
+
+                                val builderExcluir = AlertDialog.Builder(this@ListActivity)
+                                builderExcluir.setTitle(getString(R.string.atencao))
+                                builderExcluir.setMessage(getString(R.string.mensagem_nota_fiscal_excluida_sucesso))
+                                builder.setPositiveButton(getString(R.string.ok)) {dialogExcluir, witch ->
+
+                                }
+
+                                val dialogExcluir = builderExcluir.create()
+                                dialogExcluir.show()
+                            } else {
+                                val buildErro = AlertDialog.Builder(this@ListActivity)
+                                buildErro.setTitle(getString(R.string.erro_firebase))
+                                buildErro.setMessage(it.exception?.message)
+                                buildErro.setPositiveButton(getString(R.string.ok)) { dialogErro, witch ->
+
+                                }
+                                val dialogErro = buildErro.create()
+                                dialogErro.show()
+                            }
                         }
                     }
+
+                    builder.setNeutralButton("Cancel") { dialog, witch ->
+
+                    }
+
+                    val dialog: AlertDialog = builder.create()
+
+                    dialog.show()
                 }
 
                 //pogramando botao editar
@@ -91,6 +128,31 @@ class ListActivity : AppCompatActivity() {
                 }
             }
         }
+        query.addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                if (!dataSnapshot.exists()) {
+
+                    val builder = AlertDialog.Builder(this@ListActivity)
+
+                    builder.setTitle(getString(R.string.atencao))
+                    builder.setMessage(getString(R.string.mensagem_nao_tem_nota))
+                    builder.setPositiveButton(getString(R.string.ok)) { dialog, which ->
+
+                        val intent = Intent(this@ListActivity, MainActivity::class.java)
+                        startActivity(intent)
+                        this@ListActivity.finish()
+                    }
+
+                    val dialog: AlertDialog = builder.create()
+
+                    dialog.show()
+                }
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+
+            }
+        })
 
         rvNotaFiscal.adapter = FirebaseRecyclerAdapter
         rvNotaFiscal.layoutManager = LinearLayoutManager(this, OrientationHelper.VERTICAL, false )
