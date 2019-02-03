74443f362021878ac5eb3980edbbff80f1fb771f
Augusto César Favaretto Lucianetti <augusto_lucianetti@hotmail.de>
2019-02-03 13:22:27 -0200

    inserirndo nome do usuário no momento do login

--- app/src/main/java/augustolucianetti/com/br/notafiscalapplication/MainActivity.kt
+++ app/src/main/java/augustolucianetti/com/br/notafiscalapplication/MainActivity.kt
@@ -3,6 +3,12 @@ package augustolucianetti.com.br.notafiscalapplication
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import br.com.augustolucianetti.calculaflex.model.User
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -11,6 +17,19 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val query = FirebaseDatabase.getInstance().getReference(getString(R.string.nomeTabelaUsuario)).orderByChild("userId").equalTo(FirebaseAuth.getInstance().currentUser?.uid)
+        query.addListenerForSingleValueEvent(object: ValueEventListener {
+            override fun onCancelled(p0: DatabaseError) {
+
+            }
+
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                dataSnapshot.children.forEach {
+                    val user =  it.getValue(User::class.java)
+                    tvNomeUsuario.setText(getString(R.string.olah) + user?.name)
+                }
+            }
+        })
         btnCadastrar.setOnClickListener {
             val intent = Intent(this, FormActivity::class.java)
             startActivity(intent)
--- app/src/main/java/augustolucianetti/com/br/notafiscalapplication/model/User.kt
+++ app/src/main/java/augustolucianetti/com/br/notafiscalapplication/model/User.kt
@@ -3,5 +3,6 @@ package br.com.augustolucianetti.calculaflex.model
 data class User (
     val name: String = "",
     val email: String = "",
-    val phone: String = ""
+    val phone: String = "",
+    val userId: String = ""
 )
